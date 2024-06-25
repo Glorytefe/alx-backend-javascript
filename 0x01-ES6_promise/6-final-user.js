724c5f6fde25c0765164cf37ebd416fb46be8acd
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-06-26 00:25:35 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result.map(({ status, value }) => ({ status, value })));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) => result.map(({ status, value, reason }) => ({
+    status,
+    value: status === 'fulfilled' ? value : String(reason),
+  })));
 }
