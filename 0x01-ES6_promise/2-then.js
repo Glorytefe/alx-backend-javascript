6d9daa61f2d42cd8159749cacf14552e42822359
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-06-25 23:49:27 +0100

    task 2 fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   if (promise instanceof Promise) {
     promise
-      .then(() => {
+      .then(() => ({
+        status: 200,
+        body: 'success',
+      }))
+      .catch(() => new Error()).finally(() => {
         console.log('Got a response from the API');
-        return {
-          status: 200,
-          body: 'success',
-        };
-      })
-      .catch(() => {
-        console.log('Got a response from the API');
-        return new Error();
       });
   }
 }
