2e24bb78f3481ab996af4cd85969f2b5b061e04f
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-08-09 22:56:22 +0100

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  /**
+   * @param {String} code
+   * @param {String} name
+   */
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new Error('name must be string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new Error('code must be string');
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
