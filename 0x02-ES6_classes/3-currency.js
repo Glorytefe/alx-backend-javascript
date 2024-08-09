14c1069bf52d5455a0e3443c113baf20e22ab4f3
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-08-09 22:01:34 +0100

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new Error('name must be string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new Error('code must be string');
+    }
+  }
+}
