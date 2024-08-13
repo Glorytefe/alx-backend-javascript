8b00341cb7645b0cb9e1d70d599eb8f1b854d79d
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-08-14 00:58:29 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   const result = [];
   const len = startString.length;
   set.forEach((element) => {
-    if (element.startsWith(startString) && typeof element !== 'string') {
+    if (element.startsWith(startString) && typeof element === 'string') {
       const str = element.substring(len);
       if (str && str !== element) {
         result.push(element.slice(len));
