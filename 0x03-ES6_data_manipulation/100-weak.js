425f52e419be5272ac1c2a88ea16f5c261db0b0c
tefeglory.official@gmail.com <tefeglory.official@gmail.com>
2024-08-14 00:49:40 +0100

    task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const queries = weakMap.get(endpoint);
+  if (queries) {
+    if (queries >= 5) throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, queries + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
