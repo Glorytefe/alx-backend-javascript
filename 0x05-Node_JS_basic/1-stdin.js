9920f025e06140e525ffe03cc413f7a65c4b35fc
Glorytefe <glorytefe@gmail.com>
2024-10-08 12:59:28 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Capture user input
 process.stdin.on('data', (data) => {
   // Output the user's name
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
 // Detect when the input stream ends (e.g., on Ctrl+D in most terminals)
